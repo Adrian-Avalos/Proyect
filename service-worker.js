8bfb8308fd519aa1eb958b7379564369a48fcbfc
Adrian-Avalos <adrianpiti28@gmail.com>
2025-04-04 09:10:58 -0400

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,24 +5,33 @@ const urlsToCache = [
   "manifest.json",
   "imagenes/icono-192.png",
   "imagenes/icono-512.png",
-  "imagenes/icono-apple.png",
-  "imagenes/icono.ico",
+  "imagenes/icono-180.png",
   "imagenes/fondo-comida.gif",
-  "sonido/sonido.wav"
+  "sonido/sonido.wav",
+  "css/estilo.css",
+  "js/app.js",
+  "datos/datos.js"
 ];
 
+// Instala y guarda en caché
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Limpia versiones viejas
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Sirve siempre desde caché
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
