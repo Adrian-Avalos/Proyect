224e99f0de0c5075970b0fcd7621b793135a2326
Adrian-Avalos <adrianpiti28@gmail.com>
2025-04-08 19:57:20 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 // © 2025 Ramón Adrian Avalos Verá (GitHub: Adrian-Avalos)
 // Prohibida la copia o uso comercial sin autorización.
-
 const CACHE_NAME = "nutricion-cache-v2";
 const urlsToCache = [
   "index.html",
